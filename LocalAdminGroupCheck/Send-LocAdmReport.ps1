bca88ec7cbb65b292d969a017d2d965878fd6e40
chida <christian@damberg.org>
2024-10-06 22:39:54 +0200

    Update

---
+++ LocalAdminGroupCheck/Send-LocAdmReport.ps1
@@ -0,0 +1,82 @@
+﻿# script variables
+$scriptversion = '1.0'
+$scriptname = $MyInvocation.MyCommand.Name
+$ResultColl = @()
+$dbserver = 'cm01'
+$exclude = @("Administrator","CM_CP","Domain Admins")
+
+# modules
+
+if (-not (Get-PackageProvider -name NuGet))
+{
+	Install-PackageProvider nuget -ErrorAction SilentlyContinue
+}
+
+
+$checkfordbtools = Get-Module dbatools -ListAvailable
+
+if (-not $checkfordbtools.name -eq 'dbatools')
+{
+    #Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+	#Install-Module dbatools -ErrorAction SilentlyContinue -Force
+	write-host 'Dbtools not installed'
+}
+
+if (-not (Get-Module -name send-mailkitmessage))
+{
+	Install-Module send-mailkitmessage -ErrorAction SilentlyContinue
+	Import-Module send-mailkitmessage -Force
+}
+
+
+if (-not (Get-Module -name PSWriteHTML))
+{
+	Install-Module PSWriteHTML -ErrorAction SilentlyContinue
+	Import-Module PSWriteHTML -Force
+}
+
+
+        $query = "SELECT TOP (1000) [ResourceID]
+              ,[GroupID]
+              ,[RevisionID]
+              ,[AgentID]
+              ,[TimeStamp]
+              ,[Domain0]
+              ,[LocalSecurityGroup0]
+              ,[PrimaryKey0]
+              ,[SID0]
+              ,[User0]
+          FROM [CM_PS1].[dbo].[v_GS_LOCALSECURITYGROUPINVENTORY]"
+                           
+            	$data = Invoke-Sqlcmd -ServerInstance $dbserver -Database CM_PS1 -Query $query
+
+
+                ForEach ($device in $data)
+
+                {
+
+                    $resourceid = $device.ResourceID
+
+                    $querydevice = "SELECT SMS_R_System.Name0 AS ClientName FROM v_R_System AS SMS_R_System WHERE SMS_R_System.ResourceID = $resourceid";
+
+                    $clientname = Invoke-Sqlcmd -ServerInstance $dbserver -Database CM_PS1 -Query $querydevice
+                    $clientnameToString = $clientname.ClientName
+                   
+                    	$object = New-Object -TypeName PSObject
+				        $object | Add-Member -MemberType NoteProperty -Name 'Clientname' -Value $clientnameToString
+				        $object | Add-Member -MemberType NoteProperty -Name 'AccountType' -Value $device.Domain0
+                        $object | Add-Member -MemberType NoteProperty -Name 'GroupName' -Value $device.LocalSecurityGroup0
+                        $object | Add-Member -MemberType NoteProperty -Name 'SID' -Value $device.SID0
+                        $object | Add-Member -MemberType NoteProperty -Name 'User' -Value $device.User0
+
+
+				        $resultColl += $object
+
+
+                }
+                            
+
+
+$filteredArray = $ResultColl | Where-Object { $exclude -notcontains $_.user}
+
+$filteredArray | Out-GridView
---
+++ minVersionLocalAdmin.ps1
@@ -0,0 +1,32 @@
+﻿# Function to get local group members
+function Get-LocalGroupMembers {
+    param (
+        [string]$GroupName = "Administrators",
+        [string]$ComputerName = $env:COMPUTERNAME
+    )
+
+    # Get the local group
+    $group = Get-wmiobject -ClassName Win32_Group -Filter "Name='$GroupName'" -ComputerName $ComputerName
+
+    #$group = Get-CimInstance -ClassName Win32_Group -Filter "Name='$GroupName'" -ComputerName $ComputerName
+
+
+    # Get the members of the local group
+    $members = $group.GetRelated("Win32_UserAccount") + $group.GetRelated("Win32_Group")
+
+    # Create a custom object for each member
+    $members | ForEach-Object {
+        [PSCustomObject]@{
+            Name         = $_.Name
+            Domain       = $_.Domain
+            LocalAccount = $_.LocalAccount
+            SID          = $_.SID
+        }
+    }
+}
+
+# Get the members of the Administrators group
+$adminMembers = Get-LocalGroupMembers -GroupName "Administrators"
+
+# Output the results
+$adminMembers | Format-Table -AutoSize
